7a02148b00fcf30da73d46db6bd5749e9c13fe6e
Mikhail Prytychenko <nightsta1kerhf@gmail.com>
2021-11-12 16:45:01 +0200

    v.1.0.9

--- contacts.js
+++ contacts.js
@@ -1,23 +1,54 @@
-// contacts.js
+const { readFile, writeFile } = require('fs').promises;
+const { randomUUID } = require('crypto');
+const path = require('path');
 
-/*
- * Раскомментируй и запиши значение
- * const contactsPath = ;
- */
+const contactsPath = path.relative(__dirname, 'db/contacts.json');
 
-// TODO: задокументировать каждую функцию
-function listContacts() {
-  // ...твой код
+async function listContacts() {
+  const data = await readFile(contactsPath, 'utf-8');
+  return  JSON.parse(data);
 }
 
-function getContactById(contactId) {
-  // ...твой код
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+
+    for (const el of contacts) {
+      if (el.id == contactId) {
+        return el;
+      }
+    }
+
+  throw new Error(`Contact with id: ${contactId} not found. Try different id.`)
 }
 
-function removeContact(contactId) {
-  // ...твой код
+async function addContact(name, email, phone) {
+    if (!name || !email || !phone) {
+        throw new Error('Please fill all fields to add a new contact!')
+    }
+
+  const contacts = await listContacts();
+  const id = randomUUID();
+  const newContacts = contacts.concat({ id, name, email, phone });
+  await writeFile(contactsPath, JSON.stringify(newContacts));
+  return newContacts;
 }
 
-function addContact(name, email, phone) {
-  // ...твой код
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+
+  if (contacts.some(el => el.id == contactId)) {
+    const filteredContacts = contacts.filter(el => el.id != contactId);
+    await writeFile(contactsPath, JSON.stringify(filteredContacts))
+    return filteredContacts;
+  }
+
+  throw new Error(`Contact with id: ${contactId} not found. Try different id.`)
+}
+
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact
 }
